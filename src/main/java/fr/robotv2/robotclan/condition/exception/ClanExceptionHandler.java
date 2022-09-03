fbe35bcc89f4be2c6c22d5b7225140191a7b9b44
Robotv2 <81443374+Robotv2@users.noreply.github.com>
2022-09-03 10:14:05 +0200

    Fixed stack trace being printed in console.

--- src/main/java/fr/robotv2/robotclan/condition/exception/ClanExceptionHandler.java
+++ src/main/java/fr/robotv2/robotclan/condition/exception/ClanExceptionHandler.java
@@ -2,17 +2,13 @@ package fr.robotv2.robotclan.condition.exception;
 
 import org.bukkit.ChatColor;
 import org.jetbrains.annotations.NotNull;
-import revxrsal.commands.bukkit.exception.BukkitExceptionAdapter;
 import revxrsal.commands.command.CommandActor;
 import revxrsal.commands.exception.CommandExceptionHandler;
 
-public class ClanExceptionHandler extends BukkitExceptionAdapter implements CommandExceptionHandler {
+public class ClanExceptionHandler implements CommandExceptionHandler {
 
     @Override
     public void handleException(@NotNull Throwable throwable, @NotNull CommandActor actor) {
-
-        super.handleException(throwable, actor);
-
         if(throwable instanceof NotInClanException) {
             actor.reply(ChatColor.RED + "You aren't in any clan.");
             return;
